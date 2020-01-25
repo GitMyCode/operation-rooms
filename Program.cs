285f7681e4c974f856c8c0f5dc83fc383279aa36
martin.bouchard <martin.bouchard@unity3d.com>
2020-01-25 12:10:48 -0500

    refresh le projet en dotnetcore 3

---
+++ Program.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using OfficeOpenXml;
+
+namespace med_room
+{
+    public class Program
+    {
+        public static decimal[] TimeSlot = new decimal[]
+        {
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            8.0m,
+            8.0m,
+            8.0m,
+            8.0m,
+            8.0m,
+            8.0m,
+            8.0m,
+            8.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            16.0m,
+            8.0m,
+            8.0m,
+            8.0m,
+            8.0m,
+            8.0m
+        };
+
+        static void Main(string[] args)
+        {
+            var readFilePath = string.Empty;
+            var writeFilePath = string.Empty;
+#if DEBUG
+            readFilePath = "OperationTimeSlot.xlsx";
+#else
+            Console.WriteLine(@"Path du fichier a lire (ex: C:\Users\blabla\Desktop\Test.xlsx)");
+            readFilePath = Console.ReadLine();
+            Console.WriteLine(@"Path du fichier ou ecrire les resultats (ex: C:\Users\blabla\Desktop\resultat.xlsx)");
+            writeFilePath = Console.ReadLine();
+#endif
+
+
+            var fileInfo = new FileInfo(readFilePath);
+            var operationList = GetOperations(fileInfo);
+            var weekList = GetWeeks(fileInfo);
+
+            var solver = new RoomSolver();
+            var allFittedOperations = new List<OperationResult>();
+            foreach (var week in weekList)
+            {
+                var subset = operationList
+                    .Where(x => x.SemaineDispo <= week.Number)
+                    .Where(x => allFittedOperations.All(y => y.Id != x.Id))
+                    .OrderByDescending(x => x.ScoreOp)
+                    .ThenBy(x => x.Duree).ToList();
+
+                var weekSelectedOperations = solver.Solve(week, subset);
+                Console.WriteLine($"-------------------------WEEK {week.Number}-------------------------------");
+                foreach (var selectedOperation in weekSelectedOperations)
+                {
+                    Console.WriteLine(
+                        $"Id: {selectedOperation.Id} "
+                        + $"\t Score_op: {selectedOperation.ScoreOp} "
+                        + $"\t Durée: {selectedOperation.Duree} "
+                        + $"\t Limite_VAR: {selectedOperation.LimitVar}");
+                }
+
+                Console.WriteLine(
+                    $"Nb fitted operation: {weekSelectedOperations.Count} "
+                    + $"\t Total hours: {(decimal)weekSelectedOperations.Sum(x => x.Duree) / 100}"
+                    + $"\t Total ScoreOp: {weekSelectedOperations.Sum(x => x.ScoreOp)}");
+
+                allFittedOperations.AddRange(weekSelectedOperations);
+            }
+
+            Console.WriteLine($"--------------------------------------------------------");
+            Console.WriteLine($"--------------------------------------------------------");
+            Console.WriteLine($"------------------------END-----------------------------");
+            Console.WriteLine($"--------------------------------------------------------");
+            Console.WriteLine($"--------------------------------------------------------");
+            Console.WriteLine(
+                $"Nb fitted operation: {allFittedOperations.Count} "
+                + $"\t Total hours: {(decimal)allFittedOperations.Sum(x => x.Duree) / 100}"
+                + $"\t Total ScoreOp: {allFittedOperations.Sum(x => x.ScoreOp)}");
+
+#if !DEBUG
+            SaveResults(new FileInfo(writeFilePath), allFittedOperations);
+#endif
+            Console.Read();
+        }
+
+        private static List<Operation> GetOperations(FileInfo fileInfo)
+        {
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                var operationWorksheet = package.Workbook.Worksheets["Operation"];
+                var header = ExcelHelper.GetExcelHeader(operationWorksheet, 1);
+
+                var list = new List<Operation>();
+                for (var i = 2; i <= operationWorksheet.Dimension.Rows; i++)
+                {
+                    list.Add(
+                        new Operation
+                        {
+                            Id = operationWorksheet.Cells[i, header["ID_instance"]].GetValue<int>(),
+                            LimitVar = operationWorksheet.Cells[i, header["Limite_VAR"]].GetValue<string>(),
+                            ScoreOp = operationWorksheet.Cells[i, header["Score_op"]].GetValue<decimal>(),
+                            SemaineDispo = operationWorksheet.Cells[i, header["Sem_dispo_ajus"]].GetValue<int>(),
+                            Duree = (int)(100 * operationWorksheet.Cells[i, header["Duree_salle_aj_inter"]].GetValue<decimal>()),
+                        });
+                }
+
+                return list;
+            }
+        }
+
+
+
+        private static List<Week> GetWeeks(FileInfo fileInfo)
+        {
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                var operationWorksheet = package.Workbook.Worksheets["TimeSlot"];
+                var header = ExcelHelper.GetExcelHeader(operationWorksheet, 1);
+
+                var list = new List<Week>();
+                for (var i = 2; i <= operationWorksheet.Dimension.Rows; i++)
+                {
+                    var nbSlot = operationWorksheet.Cells[i, header["Nb_plages"]].GetValue<int>();
+                    var timeSlot = Enumerable.Range(0, nbSlot).Select(
+                        x => new TimeSlot
+                        {
+                            RemainingTime = 800
+                        }).ToList();
+
+                    list.Add(
+                        new Week()
+                        {
+                            Number = operationWorksheet.Cells[i, header["Week"]].GetValue<int>(),
+                            TimeSlots = timeSlot
+                        });
+                }
+
+                return list;
+            }
+        }
+
+        private static void SaveResults(FileInfo fileInfo, IList<OperationResult> results)
+        {
+            using (ExcelPackage package = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Result");
+
+                worksheet.Cells[1, 1].Value = "ID_instance";
+                worksheet.Cells[1, 2].Value = "WeekFitted";
+                worksheet.Cells[1, 3].Value = "Limite_VAR";
+                worksheet.Cells[1, 4].Value = "Score_op";
+                worksheet.Cells[1, 5].Value = "Sem_dispo_ajus";
+                worksheet.Cells[1, 6].Value = "Duree_salle_aj_inter";
+
+
+                for (int i = 0; i < results.Count; i++)
+                {
+                    var op = results[i];
+                    var row = i + 2;
+                    worksheet.Cells[row, 1].Value = op.Id;
+                    worksheet.Cells[row, 2].Value = op.WeekFitted;
+                    worksheet.Cells[row, 3].Value = op.LimitVar;
+                    worksheet.Cells[row, 4].Value = op.ScoreOp;
+                    worksheet.Cells[row, 5].Value = op.SemaineDispo;
+                    worksheet.Cells[row, 6].Value = op.Duree;
+                }
+
+
+                package.Save();
+            }
+        }
+    }
+
+    public class Week
+    {
+        public int Number { get; set; }
+
+        public IList<TimeSlot> TimeSlots { get; set; }
+    }
+
+    public class TimeSlot
+    {
+        public string Type { get; set; }
+
+        public int RemainingTime { get; set; }
+    }
+
+    public class Operation
+    {
+        public int Id { get; set; }
+
+        public string LimitVar { get; set; }
+
+        public decimal ScoreOp { get; set; }
+
+        public int SemaineDispo { get; set; }
+
+        public int Duree { get; set; }
+    }
+
+    public class OperationResult
+    {
+        public int Id { get; set; }
+
+        public string LimitVar { get; set; }
+
+        public decimal ScoreOp { get; set; }
+
+        public int SemaineDispo { get; set; }
+
+        public int Duree { get; set; }
+
+        public int WeekFitted { get; set; }
+    }
+}
+
+public static class ExcelHelper
+{
+    public static Dictionary<string, int> GetExcelHeader(ExcelWorksheet workSheet, int rowIndex)
+    {
+        var header = new Dictionary<string, int>();
+        if (workSheet != null)
+        {
+            for (var columnIndex = workSheet.Dimension.Start.Column; columnIndex <= workSheet.Dimension.End.Column; columnIndex++)
+            {
+                if (workSheet.Cells[rowIndex, columnIndex].Value != null)
+                {
+                    var columnName = workSheet.Cells[rowIndex, columnIndex].Value.ToString();
+
+                    if (!header.ContainsKey(columnName) && !string.IsNullOrEmpty(columnName))
+                    {
+                        header.Add(columnName, columnIndex);
+                    }
+                }
+            }
+        }
+
+        return header;
+    }
+}
---
+++ RoomSolver.cs
@@ -0,0 +1,197 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace med_room
+{
+    public class RoomSolver
+    {
+        public RoomSolver()
+        {
+        }
+
+        public decimal Result { get; set; }
+
+        public IList<Operation> SolveTimeSlot(IList<Operation> operations, int time)
+        {
+            var allLimit = operations.Select(x => x.LimitVar).Distinct();
+            var allMemo = new List<decimal[,]>();
+            var bestSoFar = 0.0m;
+            decimal[,] bestMemo = null;
+            var limitForBestMemo = string.Empty;
+            foreach (var limit in allLimit)
+            {
+                var operationForLimit = operations.Where(x => x.LimitVar == limit).ToList();
+                var memo = this.PackIteratif(operationForLimit, operationForLimit.Count - 1, time);
+                allMemo.Add(memo);
+                var result = memo[operationForLimit.Count - 1, time];
+                if (result > bestSoFar)
+                {
+                    bestSoFar = result;
+                    bestMemo = memo;
+                    limitForBestMemo = limit;
+                }
+            }
+
+            var list = new List<Operation>();
+            var operationForSameLimit = operations.Where(x => x.LimitVar == limitForBestMemo).ToList();
+            this.RetriedFittedOperations(bestMemo, list, operationForSameLimit, operationForSameLimit.Count, time);
+
+            return list;
+        }
+
+        public IList<OperationResult> Solve(Week week, IList<Operation> operations)
+        {
+            //var ordered = this._operations
+            //    .OrderByDescending()
+            // this.Result = this.Pack(this._operations, this._operations.Count - 1, this._timeSlot.RemainingTime, this._timeSlot.Type);
+            var selectedOperations = new List<Operation>();
+            foreach (var weekTimeSlot in week.TimeSlots)
+            {
+                var operationAvailable = operations.Where(x => selectedOperations.All(y => y.Id != x.Id)).ToList();
+                var timeSlotOperationBatch = this.SolveTimeSlot(operationAvailable, weekTimeSlot.RemainingTime);
+                selectedOperations.AddRange(timeSlotOperationBatch);
+            }
+
+            return selectedOperations.Select(x => new OperationResult
+            {
+                Duree = x.Duree,
+                Id = x.Id,
+                LimitVar = x.LimitVar,
+                ScoreOp = x.ScoreOp,
+                SemaineDispo = x.SemaineDispo,
+                WeekFitted = week.Number
+            }).ToList();
+        }
+
+        public decimal[,] PackIteratif(IList<Operation> operations, int index, int timeRemaining)
+        {
+            var memo = new decimal[operations.Count + 1, timeRemaining + 1];
+
+            for (var i = 1; i <= operations.Count; i++)
+            {
+                for (var j = 0; j <= timeRemaining; j++)
+                {
+                    if (operations[i - 1].Duree <= j)
+                    {
+                        memo[i, j] = Math.Max(
+                            memo[i - 1, j],
+                            memo[i - 1, j - operations[i - 1].Duree] + operations[i - 1].ScoreOp);
+                    }
+                    else
+                    {
+                        memo[i, j] = memo[i - 1, j];
+                    }
+                }
+            }
+
+            return memo;
+        }
+
+        private void RetriedFittedOperations(decimal[,] memo, IList<Operation> retrievedOperations, IList<Operation> allOperations, int index, int time)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+
+            if (memo[index, time] > memo[index - 1, time])
+            {
+                retrievedOperations.Add(allOperations[index - 1]);
+                this.RetriedFittedOperations(memo, retrievedOperations, allOperations, index - 1, time - allOperations[index - 1].Duree);
+            }
+            else
+            {
+                this.RetriedFittedOperations(memo, retrievedOperations, allOperations, index - 1, time);
+            }
+        }
+
+        private decimal Pack(decimal[,] memo, IList<Operation> operations, int index, int timeRemaining, string type)
+        {
+            if (index < 0)
+            {
+                return 0;
+            }
+
+            if (timeRemaining <= 0)
+            {
+                return 0;
+            }
+
+            if (memo[index, timeRemaining] != -1)
+            {
+                return memo[index, timeRemaining];
+            }
+
+            var result = 0.0m;
+            for (var time = 0; time < timeRemaining; time++)
+            {
+                var resultForThisTime = 0.0m;
+
+                // si je peux placer cette operation a ce temps
+                if (time >= operations[index].Duree)
+                {
+                    var scoreOp = operations[index].ScoreOp;
+                    var timeAfterOperation = time - operations[index].Duree;
+
+                    var resultIfWeSkipIt = this.Pack(memo, operations, index - 1, time, type);
+                    var resultIfWeTakeIt = this.Pack(memo, operations, index - 1, timeAfterOperation, type) + scoreOp;
+
+                    resultForThisTime = Math.Max(resultIfWeSkipIt, resultIfWeTakeIt);
+                }
+                else
+                {
+                    // on peut pas le placer donc on le skip
+                    resultForThisTime = this.Pack(memo, operations, index - 1, time, type);
+                }
+
+                if (resultForThisTime > result)
+                {
+                    result = resultForThisTime;
+                }
+            }
+
+            memo[index, timeRemaining] = result;
+
+            return result;
+        }
+    }
+
+    // Mon vieu code de knapsack que j'ai fais ya longtemps
+    /*
+      public static int pack(int index,List<int[]> items, int w){
+
+        if(index == nb_items){
+            return 0;
+        }
+        if(w ==0)return 0;
+        if(index < 0){
+            return 0;
+        }
+
+
+
+        if(memo[index][w] != -1){
+            return memo[index][w];
+        }
+
+        int res =0;
+        for(int i=0; i <= w ; i++){
+            if( items.get(index)[0] <= i){
+                int w2 = i - items.get(index)[0] ;
+
+
+                res=  Math.max(pack(index-1,items,i), pack(index-1,items,w2) + items.get(index)[1] );
+
+            }else{
+                res =  pack(index -1,items, i);
+            }
+
+        }
+
+        memo[index][w] = res;
+        return res;
+    }
+     */
+}
