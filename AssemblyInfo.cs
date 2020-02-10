b700ad7f463180f628a1f777f7d77e629ac21d82
martin.bouchard <martin.bouchard@unity3d.com>
2020-02-10 17:07:01 -0500

    utilsier le gitversion task pour gerer la version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,7 +1,1 @@
 ﻿
-using System.Reflection;
-
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0-alpha.1+Branch.develop.Sha.81b70ff818379813e0cd22a21231c8b5df2ead9c")]
-
