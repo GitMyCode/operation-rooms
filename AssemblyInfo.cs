3cc9f5aa33934ddd27c5415d48e4326408287654
martin.bouchard <martin.bouchard@unity3d.com>
2020-02-09 21:34:14 -0500

    assemblyInfo version

---
+++ AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿
+using System.Reflection;
+
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyInformationalVersion("2.1.0-alpha.1+Branch.develop.Sha.81b70ff818379813e0cd22a21231c8b5df2ead9c")]
+
